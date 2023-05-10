edd44fd6a312ca41abdce7b30b28cd419db0b61e
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-05-09 18:18:22 -0700

    13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+
+/**
+* add_nodeint - adds a new node at the beginning of a list
+* Return: the address of the new element, or NULL if it failed
+* @head: pointer to list
+* @n: int to put into the new node
+**/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	(*newnode).n = n;
+	(*newnode).next = *head;
+	*head = newnode;
+	return (newnode);
+}
+
+/**
+* insert_node - inserts a number into a sorted singly linked list
+* @head: pointer to head of the list
+* @number: number to be inserted
+* Return: the address of the new node, or NULL if it failed
+**/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *current, *prox;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	(*new).n = number, (*new).next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else if (number < (**head).n)
+		return (add_nodeint(head, number));
+	else
+	{
+		current = *head, prox = (*current).next;
+		while (current != NULL)
+		{
+			if ((*current).next == NULL)
+			{
+				return (add_nodeint_end(head, number));
+			}
+			if (number == (*current).n)
+			{
+				(*new).next = prox;
+				(*current).next = new;
+				break;
+			}
+			if (number < (*prox).n && prox != NULL)
+			{
+				(*new).next = prox;
+				(*current).next = new;
+				break;
+			}
+			current = (*current).next;
+			prox = (*current).next;
+		}
+	}
+	return (new);
+}
