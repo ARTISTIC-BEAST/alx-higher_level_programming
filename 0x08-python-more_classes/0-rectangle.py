16abab7be255c54b2dea5255ff478d634ff0d6c2
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-05 11:53:37 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class.
-"""
+"""Rectangle module"""
 
 
-class Rectangle:
-    """Empty class."""
-    pass
+class Rectangle():
+    """Rectangle empty class"""
+
+    def __init__(self):
+        pass
