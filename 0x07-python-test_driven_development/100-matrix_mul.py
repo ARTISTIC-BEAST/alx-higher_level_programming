ede5f1ef0fabd5f9d76b30bc591afdaddb9e12f3
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-06 18:28:30 -0700

    100-matrix_mul

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""matrix mult module"""
+
+
+def matrix_mul(m_a, m_b):
+    """[summary]
+    Args:
+        m_a ([type]): [description]
+        m_b ([type]): [description]
+    """
+    pass
