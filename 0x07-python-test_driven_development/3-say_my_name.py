a55d56e3efdd6f75c0bfb19a240e6a56cbd9571d
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-06 18:06:24 -0700

    3-say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Say my name nmodule"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function to print full name
+    Args:
+        first_name (str): first name
+        last_name (str, optional): last name. Defaults to "".
+    Raises:
+        TypeError: first_name or last_name not a string
+    """
+    m1 = "must be a string"
+    if type(first_name) is not str:
+        raise TypeError("first_name {}".format(m1))
+    if type(last_name) is not str:
+        raise TypeError("last_name {}".format(m1))
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
