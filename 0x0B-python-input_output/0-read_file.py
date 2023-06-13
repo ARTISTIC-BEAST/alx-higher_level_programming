c2274cce6f211c2fa0eb972e562e766f2ae3317d
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-13 06:07:39 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+lename open module"""
+
+
+def read_file(filename=""):
+        """read file function
+            Args:
+                        filename (str, optional): [description]. Defaults to "".
+                            """
+                                with open(filename, "r", encoding='utf-8') as f:
+                                        print(f.read(), end="")
