ded73caf0b412c4f4f370f19e1b14d184c272d69
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-12 19:46:39 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+st module"""
+
+
+class MyList(list):
+        """MyList class
+            Args:
+                        list (list): list
+                            """
+
+                                def print_sorted(self):
+                                        """print sorted method"""
+                                                nl = self.copy()
+                                                        print(sorted(nl))
