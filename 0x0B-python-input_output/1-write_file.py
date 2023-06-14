c82daff005ccbf2dab32f632108c0196c3b22ca5
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-13 19:57:46 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,14 @@
-n a file module"""
+#!/usr/bin/python3
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-        """FUnction to write in a file
-            Args:
-                        filename (str, optional): path to file. Defaults to "".
-                                text (str, optional): characters to write. Defaults to "".
-                                    Returns:
-                                                int: number of characters written
-                                                    """
-                                                        with open(filename, "w", encoding="utf-8") as f:
-                                                                f.write(text)
-                                                                    return len(text)
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
