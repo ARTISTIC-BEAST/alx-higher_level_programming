604b99bb2d216d2f1eff2ee11b3242a6fc5be56d
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 19:13:48 -0700

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,30 +1,24 @@
-ass module"""
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-        """Empty basegeometry class"""
+    """Reprsent base geometry."""
 
-            def __init__(self):
-                    """Init function"""
-                            pass
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
 
-                                def area(self):
-                                        """area function
-                                                Raises:
-                                                                Exception: area not implemented
-                                                                        """
-                                                                                raise Exception("area() is not implemented")
-
-                                                                                    def integer_validator(self, name, value):
-                                                                                            """[summary]
-                                                                                                    Args:
-                                                                                                                    name ([type]): [description]
-                                                                                                                                value ([type]): [description]
-                                                                                                                                        Raises:
-                                                                                                                                                        TypeError: [description]
-                                                                                                                                                                    ValueError: [description]
-                                                                                                                                                                            """
-                                                                                                                                                                                    if type(value) is not int:
-                                                                                                                                                                                                raise TypeError("{} must be an integer".format(name))
-                                                                                                                                                                                                        if value <= 0:
-                                                                                                                                                                                                                    raise ValueError("{} must be greater than 0".format(name))
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
