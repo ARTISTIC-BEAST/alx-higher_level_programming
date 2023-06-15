bdb2dec39998dd8d5ce64c11bc37dd80e014e69f
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 17:28:26 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,14 @@
-
-dule to append"""
+#!/usr/bin/python3
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-        """Function to append text
-            Args:
-                        filename (str, optional): filepath . Defaults to "".
-                                text (str, optional): characters. Defaults to "".
-                                    Returns:
-                                                int: number of characters written
-                                                    """
-                                                        with open(filename, "a") as f:
-                                                                f.write(text)
-                                                                    return len(text)
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
