9491f7257fe9533695bdbbf6c68bb1339232546b
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 19:32:06 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,17 @@
-
-dule"""
+#!/usr/bin/python3
+"""Module"""
 
 
 class MyInt(int):
-        """Class"""
+    """Class"""
 
-            def __ini__(self, value):
-                    super().__init__()
+    def __ini__(self, value):
+        super().__init__()
 
-                        def __eq__(self, other):
-                                """Equal method"""
-                                        return int(self) != other
+    def __eq__(self, other):
+        """Equal method"""
+        return int(self) != other
 
-                                            def __ne__(self, other):
-                                                    """Not equal method"""
-                                                            return int(self) == other
+    def __ne__(self, other):
+        """Not equal method"""
+        return int(self) == other
