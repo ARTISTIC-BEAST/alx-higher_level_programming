3498036bb78ca1c59ec6e8c7f7a346a2d31d31e5
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 19:29:08 -0700

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,25 @@
-re module"""
+#!/usr/bin/python3
+"""Square module"""
 Rect = __import__('9-rectangle').Rectangle
 
 
 class Square(Rect):
-        """Square class"""
+    """Square class"""
 
-            def __init__(self, size):
-                    """[summary]
-                            Args:
-                                            size (int): size of square
-                                                    """
-                                                            self.integer_validator("size", size)
-                                                                    super().__init__(size, size)
-                                                                            self.__size = size
+    def __init__(self, size):
+        """[summary]
+        Args:
+            size (int): size of square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
-                                                                                def __str__(self):
-                                                                                        """overloading str method
-                                                                                                Returns:
-                                                                                                                str: square data
-                                                                                                                        """
+    def __str__(self):
+        """overloading str method
+        Returns:
+            str: square data
+        """
 
-                                                                                                                                sr = "[Square] {}/{}".format(self.__size, self.__size)
-                                                                                                                                        return sr
+        sr = "[Square] {}/{}".format(self.__size, self.__size)
+        return sr
