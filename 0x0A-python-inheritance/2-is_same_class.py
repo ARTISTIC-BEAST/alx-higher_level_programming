cba6daf46ed24db936b5077e5109daa5ab60e004
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 18:33:30 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,12 +1,13 @@
-s same class module """
+#!/usr/bin/python3
+""" is same class module """
 
 
 def is_same_class(obj, a_class):
-        """[summary]
-            Args:
-                        obj (obj): the object
-                                a_class (class): the class
-                                    Returns:
-                                                Boolean: if obj is an instance of class
-                                                    """
-                                                        return type(obj) is a_class
+    """[summary]
+    Args:
+        obj (obj): the object
+        a_class (class): the class
+    Returns:
+        Boolean: if obj is an instance of class
+    """
+    return type(obj) is a_class
