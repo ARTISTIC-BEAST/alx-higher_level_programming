955053400c8dcbb2c33c0101dc0e3e4e49c5d9b4
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 18:02:22 -0700

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""pascal traingle module"""
+
+
+def pascal_triangle(n):
+    """pascal triangle function
+    Args:
+        n ([type]): [description]
+    Returns:
+        [type]: [description]
+    """
+    if n == 0:
+        triangle = []
+    elif n == 1:
+        triangle = [[1]]
+    else:
+        triangle = [[1], [1, 1]]
+        for i in range(1, n-1):
+            floor = [1]
+            for j in range(0, len(triangle[i])-1):
+                floor.extend([triangle[i][j] + triangle[i][j+1]])
+            floor += [1]
+            triangle.append(floor)
+    return triangle
