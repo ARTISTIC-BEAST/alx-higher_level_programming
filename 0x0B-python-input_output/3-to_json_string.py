5131b5db1d8bfbfa7fb15d91e29952c9fb04a40e
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-06-14 17:29:29 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+
+ json module"""
+ import json
+
+
+ def to_json_string(my_obj):
+     """to json function
+         Args:
+                     my_obj (obj): object
+                         Returns:
+                                     json: json object
+                                         """
+                                             return json.dumps(my_obj)
