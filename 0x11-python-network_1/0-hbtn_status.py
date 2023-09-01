75944402d7a4f02cc9d38d78c32418445b7924fb
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-09-01 06:03:22 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 0. What's my status? #0 """
+
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    with urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
