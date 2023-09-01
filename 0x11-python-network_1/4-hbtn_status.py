5d90cb26dd4414c8283db09ba6cd24dd0cacbca9
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-09-01 06:14:46 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 from requests import get
 
 if __name__ == "__main__":
-    r = get('https://intranet.hbtn.io/status')
+    r = get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
