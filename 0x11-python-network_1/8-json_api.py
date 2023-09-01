234ee09d9db210f41a73a181fa942f8ea65ad231
ARTISTCI-BEAST <artisticbeast1@gmail.com>
2023-09-01 06:20:03 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" 8. Search API """
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
 
-from sys import argv
-from requests import post, get
 
 if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        _data = {'q': argv[1]}
-    except:
-        _data = {'q': ""}
-    url = 'http://0.0.0.0:5000/search_user'
-    try:
-        r = post(url, data=_data)
-        rj = r.json()
-        if (len(rj) is not 0):
-            print("[{}] {}".format(rj.get('id'), rj.get('name')))
+        response = r.json()
+        if response == {}:
+            print("No result")
         else:
-            print('No result')
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
-        print('Not a valid JSON')
+        print("Not a valid JSON")
