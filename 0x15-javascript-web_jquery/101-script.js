b95d3b6c6ee47f335d202f0e57e578a98d11803a
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-10-04 18:08:43 -0700

    101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,27 @@
+const $ = window.$;
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    document.getElementById('add_item').addEventListener('click', function () {
+      const node = document.createElement('LI');
+      const txtNode = document.createTextNode('Item');
+      node.appendChild(txtNode);
+      $('.my_list')[0].appendChild(node);
+    });
+    document.getElementById('remove_item').addEventListener('click', function () {
+      $('.my_list').children().last().remove();
+    });
+    document.getElementById('clear_list').addEventListener('click', function () {
+      $('.my_list li').remove();const $ = window.$;
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    document.getElementById('add_item').addEventListener('click', function () {
+      const node = document.createElement('LI');
+      const txtNode = document.createTextNode('Item');
+      node.appendChild(txtNode);
+      $('.my_list')[0].appendChild(node);
+    });
+    document.getElementById('remove_item').addEventListener('click', function () {
+      $('.my_list').children().last().remove();
+    });
+    document.getElementById('clear_list').addEventListener('click', function () {
+      $('.my_list li').remove();
